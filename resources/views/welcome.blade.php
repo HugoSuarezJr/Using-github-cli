26f8ec4e019fd4349bbfeb66270aef18b380e438
Hugo Suarez <hugo.suarez92@yahoo.com>
2022-07-04 02:50:48 -0400

    trying out new commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,9 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script src="https://cdn.tailwindcss.com"></script>
 
-        <title>Laravel</title>
+        <title>Laravel is cool</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -35,6 +36,8 @@
                     @endauth
                 </div>
             @endif
+
+            <img src="https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/677.jpg" alt="faker" class="w-400">
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
